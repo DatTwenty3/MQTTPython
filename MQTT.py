c3d044165b774b3c2d8f9b078fda00e02df49125
Dat Le <datle.hcmut@gmail.com>
2021-04-03 17:32:24 +0700

    Update DHT22

--- MQTT.py
+++ MQTT.py
@@ -50,29 +50,35 @@ while True:
             #MQTT publish
             client.publish("Status", "SttDV1ON")
             print("Device 1 is ON!")
-        if (dataFromArduino==b'SttDV1OFF'):
+        elif (dataFromArduino==b'SttDV1OFF'):
             #MQTT publish
             client.publish("Status", "SttDV1OFF")
             print("Device 1 is OFF!")
             
-        if (dataFromArduino==b'SttDV2ON'):
+        elif (dataFromArduino==b'SttDV2ON'):
             #MQTT publish
             client.publish("Status", "SttDV2ON")
             print("Device 2 is ON!")
-        if (dataFromArduino==b'SttDV2OFF'):
-            client.publish("Status", "SttDV2OFF")
+        elif (dataFromArduino==b'SttDV2OFF'):
             #MQTT publish
             client.publish("Status", "SttDV2OFF")
             print("Device 2 is OFF!")
+        
+        else:
+            dataFromSensor = int(dataFromArduino)
+            client.publish("AirHumi", dataFromSensor//100)
+            client.publish("Temp", dataFromSensor%100)
+            
+            
     
     #MQTT
-    i=i+1
-    client.publish("Temp", str(i))
-    client.publish("AirHumi", str(i+1))
-    client.publish("SoilMois", str(i+2))
-    client.publish("Power", str(i+3))
-    print("Message sent!")
-    time.sleep(5)
+    #i=i+1
+    #client.publish("Temp", str(i))
+    #client.publish("AirHumi", str(i+1))
+    #client.publish("SoilMois", str(i+2))
+    #client.publish("Power", str(i+3))
+    #print("Message sent!")
+    time.sleep(0.5)
     
 client.loop_stop()
 client.disconnect()
